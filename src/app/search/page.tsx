e021084c99fde4b72245d2a46d6f885ca0e1671b
ak1nttr <nsnr6458@gmail.com>
2024-07-16 16:31:32 +0300

    filter box implemented

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import HotelCard from "../../components/card/HotelCard";
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SearchBar from "@/components/SearchBar";
+import FilterSidebar from "@/app/filtering/FilterSideBar";
+import {Box} from "@mui/material";
 
 interface Hotel {
   [x: string]: any;
@@ -80,44 +81,33 @@ const HotelDetail: React.FC = () => {
   };
 
   return (
-    <div className="m-auto pt-4 items-center w-full max-w-3xl" style={{marginTop:'80px'}}>
+      <div className="m-auto pt-4 items-center w-full max-w-3xl" style={{marginTop: '80px'}}>
         <SearchBar
-            sx={{marginTop:'40px' , marginLeft:'10%', marginRight:'10%'}}
+            sx={{marginTop: '40px', marginLeft: '10%', marginRight: '10%'}}
             backgroundColor={'#F5F5F5'}
             height={'90px'}
         />
-      <div className="flex justify-between mb-4" style={ { marginTop: '80px',marginLeft:'37%'}}>
-        {/* Konum arama çubuğu */}
-        <TextField
-          type="text"
-          placeholder="Search"
-          variant="outlined"
-          size="small"
-          value={locationFilter}
-          onChange={handleLocationFilterChange}
-        />
-        {/* Filtreleme düğmesi */}
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleFilterClick}
-          sx={{ ml: 1 , marginTop:'1px' , height: '38px'}}
-        >
-          Filter
-        </Button>
+        <Box display="flex" marginTop="65px" marginLeft="10%">
+          <Box flex="1" marginRight="20px">
+            <FilterSidebar/>
+          </Box>
+          <Box flex="3" display="flex" flexDirection="column" gap="0px" >
+            {hotelData.map((hotel, index) => (
+                <Box key={index} >
+                  <HotelCard
+                      title={hotel.title}
+                      location={hotel.location}
+                      price={hotel.price}
+                      apiEndpoint={`/api/hotel/${hotel.id}`} // Örnek API endpoint'i
+                  />
+                </Box>
+            ))}
+          </Box>
+        </Box>
       </div>
-      {hotelData.map((hotel, index) => (
-        <div key={index} className="py-2" style={{marginLeft:'35%'}}>
-          <HotelCard
-            title={hotel.title}
-            location={hotel.location}
-            price={hotel.price}
-            apiEndpoint={`/api/hotel/${hotel.id}`} // Örnek API endpoint'i
-          />
-        </div>
-      ))}
-    </div>
   );
 };
 
 export default HotelDetail;
+
+
