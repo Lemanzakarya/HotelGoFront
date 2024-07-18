19aa386f088f4f388a5dec4885e2be8241e22062
Lemanzakarya <95051873+Lemanzakarya@users.noreply.github.com>
2024-07-18 15:15:27 +0300

    Added Rooms page to hotel detail page

--- src/app/hoteldetail/page.tsx
+++ src/app/hoteldetail/page.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import Amenities from '../../components/detail/Amenities';
 import Gallery from '../../components/detail/Gallery';
 import { Container, Box, Typography, Tabs, Tab, Paper } from '@mui/material';
+import Overview from '@/components/detail/Overview';
+import Rooms from '@/components/detail/Rooms';
 
 
 const HotelDetail: React.FC = () => {
@@ -29,35 +31,22 @@ const HotelDetail: React.FC = () => {
           <Tabs value={tabValue} onChange={handleChange} aria-label="hotel detail tabs">
             <Tab label="Overview" />
             <Tab label="Amenities" />
+            <Tab label="Rooms" />
           </Tabs>
         </Box>
         {tabValue === 0 && (
           <Box sx={{ mt: 2 }}>
-            <Paper elevation={3} style={{ padding: 16 }}>
-          <Typography variant="h6" component="h2" gutterBottom>
-            About the Hotel
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Granada Luxury Hotel Belek warmly welcomes those who want to experience a luxurious and fun-filled holiday in the unique sea and refreshing atmosphere of the Mediterranean.
-          </Typography>
-          <Typography variant="body1" paragraph>
-          With its stylish and comfortable rooms, the facility provides a comfortable holiday experience and is located a short distance from the sea. The facility has its own sandy beach with shuttle service to the beach. With its clear sea and giant aquapark, the facility offers summer fun for guests of all ages.
-          </Typography>
-          <Typography variant="body1" paragraph>
-          The facility stands out with its rich cuisine offering unique flavors of Turkish and world cuisines, and snack areas providing delicious snacks along with refreshing drinks throughout the day.
-          </Typography>
-          <Typography variant="body1" paragraph>
-          Granada Luxury Hotel Belek promises an unforgettable holiday experience with its expert SPA center staff offering amenities such as a Turkish bath, indoor pool, sauna, steam room, and massage services.
-          </Typography>
-          <Typography variant="h6" component="div" color="primary">
-              9.2 Wonderful
-            </Typography>
-        </Paper>
+            <Overview />
           </Box>
         )}
         {tabValue === 1 && (
           <Box sx={{ mt: 2 }}>
             <Amenities />
+          </Box>
+        )}
+         {tabValue === 2 && (
+          <Box sx={{ mt: 2 }}>
+            <Rooms />
           </Box>
         )}
       </Container>
