2f122c882557b9424c7f483b9ba22623a04447d4
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-07-19 11:37:07 +0300

    add stars to hotel detail page

--- src/app/hoteldetail/page.tsx
+++ src/app/hoteldetail/page.tsx
@@ -1,55 +1,67 @@
-'use client'
+  'use client'
 
-import React from 'react';
-import Amenities from '../../components/detail/Amenities';
-import Gallery from '../../components/detail/Gallery';
-import { Container, Box, Typography, Tabs, Tab, Paper } from '@mui/material';
-import Overview from '@/components/detail/Overview';
+  import React from 'react';
+  import Amenities from '../../components/detail/Amenities';
+  import Gallery from '../../components/detail/Gallery';
+  import { Container, Box, Typography, Tabs, Tab, Paper,Rating } from '@mui/material';
+  import Overview from '@/components/detail/Overview';
 
 
-const HotelDetail: React.FC = () => {
-  const [tabValue, setTabValue] = React.useState(0);
+  const HotelDetail: React.FC = () => {
+    const [tabValue, setTabValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setTabValue(newValue);
-  };
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setTabValue(newValue);
+    };
 
-  return (
-    <Box>
-      <Container maxWidth="lg" sx={{ mt: 12 }}>
-      <Box mt={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Granada Luxury Belek
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-          Belek, Kadriye, Antalya
-        </Typography>
-      </Box>
+    return (
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 12 }}>
+          <Box display="flex" flexDirection="row" alignItems="center" mt={4} >
+            <Box>
+              <Typography variant="h4" component="h1" gutterBottom>
+                Granada Luxury Belek
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+                Belek, Kadriye, Antalya
+              </Typography>
+            </Box>
+            <Box
+              flexGrow={1}  
+              mr={2}        
+              order={2}
+              display="flex"
+              flexDirection="row"
+              justifyContent="flex-end"
+            >
+              <Rating name="card-rating" value={5} readOnly={true} size="medium" precision={0.5} />
+            </Box>
+        </Box>
         <Gallery />
         <Box sx={{ borderBottom: 1, borderColor: 'divider', mt: 2 }}>
-          <Tabs value={tabValue} onChange={handleChange} aria-label="hotel detail tabs">
-            <Tab label="Overview" />
-            <Tab label="Amenities" />
-            <Tab label="Rooms" />
-          </Tabs>
-        </Box>
-        {tabValue === 0 && (
-          <Box sx={{ mt: 2 }}>
-            <Overview />
+            <Tabs value={tabValue} onChange={handleChange} aria-label="hotel detail tabs">
+              <Tab label="Overview" />
+                <Tab label="Amenities" />
+                <Tab label="Rooms" />
+              </Tabs>
           </Box>
-        )}
-        {tabValue === 1 && (
-          <Box sx={{ mt: 2 }}>
-            <Amenities />
-          </Box>
-        )}
-         {tabValue === 2 && (
-          <Box sx={{ mt: 2 }}>
-          </Box>
-        )}
-      </Container>
-    </Box>
-  );
-};
+            {tabValue === 0 && (
+              <Box sx={{ mt: 2 }}>
+                <Overview />
+              </Box>
+            )}
+            {tabValue === 1 && (
+              <Box sx={{ mt: 2 }}>
+                <Amenities />
+              </Box>
+            )}
+            {tabValue === 2 && (
+              <Box sx={{ mt: 2 }}>
+              </Box>
+            )}
+        </Container>
+      </Box>
+    );
+  };
 
-export default HotelDetail;
+  export default HotelDetail;
