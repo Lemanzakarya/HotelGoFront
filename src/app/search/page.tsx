e0d3f6f13ac7791bba36e91bb1d98b3a240b8271
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-07-22 16:47:39 +0300

    optimization search bar for mozilla

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,10 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import HotelCard from "../../components/card/HotelCard";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import SearchBar from "@/components/SearchBar";
 import { useMediaQuery } from "@mui/material";
 import FilterSidebar from "../../components/filtering/FilterSideBar";
-
 
 interface Hotel {
   id: string;
@@ -43,12 +42,12 @@ const hotel: Hotel[] = [
     location: "Merkez, Adana",
     price: "499,99",
     tags: ["Wifi", "Star", "2.4 km"],
-  }
+  },
 ];
 
-const SearchPage : React.FC = () => {
+const SearchPage: React.FC = () => {
   const [hotelData, setHotelData] = useState<Hotel[]>([]);
-  const isSmallScreen = useMediaQuery('(max-width:900px)');
+  const isSmallScreen = useMediaQuery("(max-width:900px)");
 
   useEffect(() => {
     fetchHotels();
@@ -59,47 +58,52 @@ const SearchPage : React.FC = () => {
     setHotelData(hotel);
   };
 
-
-
-
   return (
-      <div style={{marginTop: '20px'}}>
+    <div style={{ marginTop: "20px" }}>
+      <Box
+        sx={{
+          position: "sticky",
+          top: 4,
+          zIndex: 10,
+        }}
+      >
+        <SearchBar
+          sx={{ marginTop: "20px", marginLeft: "5%", marginRight: "5%" }}
+          backgroundColor={"#F5F5F5"}
+          height={isSmallScreen ? "100%" : 80}
+        />
+      </Box>
+      <Box
+        display="flex"
+        flexDirection={isSmallScreen ? "column" : "row"}
+        marginTop="4%"
+      >
+        <Box flex="1" marginLeft="5%">
+          <FilterSidebar />
+        </Box>
         <Box
-          sx={{
-            position:'sticky',
-            top:4,
-            zIndex: 10,
-          }}
+          flex="3"
+          display="flex"
+          flexDirection="column"
+          marginRight="5%"
+          marginLeft={isSmallScreen ? "5%" : "0%"}
         >
-            <SearchBar
-                sx={{marginTop: '20px', marginLeft: '5%', marginRight: '5%'}}
-                backgroundColor={'#F5F5F5'}
-                height={isSmallScreen ? '100%' : 80}
-            />
+          {hotelData.map((hotel, index) => (
+            <Box key={index}>
+              <HotelCard
+                title={hotel.title}
+                location={hotel.location}
+                price={hotel.price}
+                tags={hotel.tags}
+                apiEndpoint={`/api/hotel/${hotel.id}`}
+                stars={3.5}
+              />
+            </Box>
+          ))}
         </Box>
-            <Box display="flex" flexDirection={isSmallScreen ? 'column' : 'row'} marginTop="4%">
-                <Box flex="1" marginLeft="5%" >
-            <FilterSidebar/>
-          </Box>
-          <Box flex="3" display="flex" flexDirection="column" marginRight="5%" marginLeft={isSmallScreen? "5%" : "0%"}>
-            {hotelData.map((hotel, index) => (
-                <Box key={index} >
-                  <HotelCard
-                      title={hotel.title}
-                      location={hotel.location}
-                      price={hotel.price}
-                      tags={hotel.tags}
-                      apiEndpoint={`/api/hotel/${hotel.id}`}
-                      stars={3.5}
-                  />
-                </Box>
-            ))}
-          </Box>
-        </Box>
-      </div>
+      </Box>
+    </div>
   );
 };
 
 export default SearchPage;
-
-
