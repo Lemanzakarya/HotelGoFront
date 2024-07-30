9bea4c86aa9889f7a6515a4e2bdc0bf014e2f485
Lemanzakarya <95051873+Lemanzakarya@users.noreply.github.com>
2024-07-30 10:50:33 +0300

    Nights,default thumbnail and currency added to hotel card

--- src/components/card/HotelCard.tsx
+++ src/components/card/HotelCard.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import AspectRatio from '@mui/joy/AspectRatio';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
@@ -9,35 +9,35 @@ import Button from '@mui/joy/Button';
 import { useMediaQuery, Rating } from '@mui/material';
 import { useRouter } from 'next/navigation';
 
-
 interface HotelCardProps {
   title: string | undefined;
   location: string | undefined;
   price: string | undefined;
-  stars : string | undefined;
+  stars: string | undefined;
   isLoading: boolean | undefined;
   thumbnail: string | undefined;
   nights: number | undefined;
-  currency : string | undefined;
-  setIsLoading : React.Dispatch<React.SetStateAction<boolean>>
+  currency: string | undefined;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const HotelCard: React.FC<HotelCardProps> = ({
   title,
   location,
   price: initialPrice,
-  stars = 0,
+  stars = '0',
   isLoading,
   thumbnail,
   nights,
   currency,
-  setIsLoading
+  setIsLoading,
 }) => {
   const [price, setPrice] = useState<string>(initialPrice || '');
   const isSmallScreen = useMediaQuery('(max-width:900px)');
   const router = useRouter();
 
-  const starnum = parseInt(String(stars));
+  const starnum = parseInt(stars);
+  const defaultThumbnail = 'https://orinter.com.br/public/img/hotel-default.jpg';
 
   const handleLookThrough = () => {
     setIsLoading(true);
@@ -47,23 +47,22 @@ const HotelCard: React.FC<HotelCardProps> = ({
     }, 2000);
   };
 
-
   return (
     <Card
-      orientation={isSmallScreen ? "vertical" : "horizontal"}
+      orientation={isSmallScreen ? 'vertical' : 'horizontal'}
       variant="outlined"
       sx={{
         width: 'auto',
-        m:1,
+        m: 1,
         display: 'flex',
-        alignContent:isSmallScreen ? 'center' : 'unset',
+        alignContent: isSmallScreen ? 'center' : 'unset',
         flexDirection: isSmallScreen ? 'column' : 'row',
       }}
     >
       <CardOverflow>
-        <AspectRatio ratio={isSmallScreen ? 2 : 1} sx={{ width: isSmallScreen ? '200' : 200 }} >
+        <AspectRatio ratio={isSmallScreen ? 2 : 1} sx={{ width: isSmallScreen ? 550 : 230 }}>
           <img
-            src={thumbnail}
+            src={thumbnail || defaultThumbnail}
             alt={title}
             loading="lazy"
             style={{
@@ -72,27 +71,40 @@ const HotelCard: React.FC<HotelCardProps> = ({
           />
         </AspectRatio>
       </CardOverflow>
-      <CardContent sx={{ flex: '1 0 auto', ml:1}} >
-        <Typography fontWeight="bold" textColor="text.primary" fontSize={27}>
-          {title}
+      <CardContent sx={{ flex: '1 0 auto', ml: 1 }}>
+        <Typography fontWeight="bold" textColor="text.primary" fontSize={25}>
+          {title || 'Title Missing'}
         </Typography>
-        <Rating name=" card-rating"  value={starnum} readOnly={true} size="medium"  precision={0.5}/>
-        <Typography textColor="text.secondary" mt={5}>
-          {location}
+        <Rating name="card-rating" value={starnum} readOnly size="medium" precision={0.5} />
+        <Typography textColor="text.secondary" mt={2}>
+          {location || 'Location Missing'}
+        </Typography>
+        <Typography textColor="text.secondary" mt={2}>
+          {currency ? `Currency: ${currency}` : 'Currency Missing'}
+        </Typography>
+        <Typography textColor="black" sx={{ mt: 1, fontSize: '1.3rem' }}>
+          {nights !== undefined ? `Nights: ${nights}` : 'Nights Missing'}
         </Typography>
       </CardContent>
       <CardContent sx={{}}>
-      <Typography fontWeight="bold" textColor="success.plainColor" sx={{ mt:4 ,mr:1 , alignSelf: 'flex-end', fontSize : 30 }}>
-          {price} $
+        <Typography fontWeight="bold" textColor="success.plainColor" sx={{ mt: 4, mr: 1, alignSelf: 'flex-end', fontSize: 28 }}>
+          {price || 'Price Missing'} {currency || '$'}
         </Typography>
         <Button
           variant="solid"
           size="lg"
-          sx={{ mt: 3, width: isSmallScreen ? '100%' : 'auto', alignSelf: isSmallScreen ? 'center' : 'flex-end',backgroundColor:'orange','&:hover': { backgroundColor: 'darkorange' }}}
+          sx={{
+            mt: 4,
+            mr: 1,
+            width: isSmallScreen ? '100%' : 'auto',
+            alignSelf: isSmallScreen ? 'center' : 'flex-end',
+            backgroundColor: 'orange',
+            '&:hover': { backgroundColor: 'darkorange' },
+          }}
           onClick={handleLookThrough}
           disabled={isLoading}
         >
-          Look Through
+          {isLoading ? 'Loading...' : 'Look Through'}
         </Button>
       </CardContent>
     </Card>
